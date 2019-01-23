0b76758555cea28497b997a96243c12309396a28
Rafael Munoz <rafa@bitbot.com>
2019-01-22 16:25:45 -0800

    Update Snackbar message.

--- app/src/main/java/com/example/tshk/testproject/MainActivity.java
+++ app/src/main/java/com/example/tshk/testproject/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hi, there!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
